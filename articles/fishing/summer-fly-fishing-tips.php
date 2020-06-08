13e28d6360756464fac3328b5306d56e98d57f92
Tucker Brady <tuck.al.brady@gmail.com>
2020-06-07 19:07:27 -0600

    Created summer fly fishing article

---
+++ articles/fishing/summer-fly-fishing-tips.php
@@ -0,0 +1,303 @@
+<!DOCTYPE php>
+<php class="no-js" lang="en">
+<head>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-165915729-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-165915729-1');
+    </script>
+
+    <!--- basic page needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>Summer Fly Fishing Tips - Fishing</title>
+    <meta name="keywords" content="Hunting, Fishing, Camping, Outdoors">
+    <meta name="description" content="Summer Fly Fishing Tips">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Mac Brady">
+    <base href="https://mtnoutlaw.com">
+
+
+    <!-- mobile specific metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="/css/base.css">
+    <link rel="stylesheet" href="/css/vendor.css">
+    <link rel="stylesheet" href="/css/main.css">
+
+    <!-- script
+    ================================================== -->
+    <script src="js/modernizr.js"></script>
+    <script src="js/pace.min.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js?render=6LfHgvYUAAAAAJHWyCyPdi4YLNNCKJcnr9gegH8n"></script>
+    <script>
+        grecaptcha.ready(function () {
+            grecaptcha.execute('6LfHgvYUAAAAAJHWyCyPdi4YLNNCKJcnr9gegH8n', { action: 'contact' }).then(function (token) {
+                var recaptchaResponse = document.getElementById('recaptchaResponse');
+                recaptchaResponse.value = token;
+            });
+        });
+    </script>
+
+    <!-- favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+</head>
+
+<body id="top">
+
+    <!-- pageheader
+    ================================================== -->
+    <div class="s-pageheader">
+
+        <header class="header">
+            <div class="header__content row">
+
+                <div class="header__logo">
+                    <a class="logo" href="/">
+                        <img src="/images/page-logo.png" alt="Homepage">
+                    </a>
+                </div> <!-- end header__logo -->
+
+                <a class="header__toggle-menu" href="#0" title="Menu"><span>Menu</span></a>
+
+                <nav class="header__nav-wrap">
+
+                    <ul class="header__nav">
+                        <li><a href="/" title="">Home</a></li>
+                        <li class="has-children, current">
+                            <a href="#" title="">Articles</a>
+                            <ul class="sub-menu">
+                                <li><a href="articles/clothing/clothing-articles.php">Clothing</a></li>
+                                <li><a href="articles/fishing/fishing-articles.php">Fishing</a></li>
+                                <li><a href="articles/gear/gear-articles.php">Gear</a></li>
+                                <li><a href="articles/hunting/hunting-articles.php">Hunting</a></li>
+                                <li><a href="articles/lifestyle/lifestyle-articles.php">Lifestyle</a></li> 
+                                <li><a href="articles/outdoor-adventure/outdoor-adventure-articles.php">Outdoor Adventure</a></li>       
+                            </ul>
+                        </li>
+                        <li><a href="huskydivision.php" title="">Husky Division</a></li>
+                        <li><a href="about.php" title="">About</a></li>
+                        <li><a href="contact.php" title="">Contact</a></li>
+                    </ul> <!-- end header__nav -->
+
+                    <a href="#0" title="Close Menu" class="header__overlay-close close-mobile-menu">Close</a>
+
+                </nav> <!-- end header__nav-wrap -->
+
+            </div> <!-- header-content -->
+        </header> <!-- header -->
+
+    </div> <!-- end s-pageheader -->
+
+
+    <!-- s-content
+    ================================================== -->
+    <section class="s-content s-content--narrow s-content--no-padding-bottom">
+
+        <article class="row format-standard">
+
+            <div class="s-content__header col-full">
+                <h1 class="s-content__header-title">
+                    Looking to land that big lunker? Or just trying to rip some lips in general, let's break it down.
+                </h1>
+                <ul class="s-content__header-meta">
+                    <li class="date">June 7, 2020</li>
+                    <li class="cat">
+                        In
+                        <a href="/ar">Fishing</a>
+                    </li>
+                </ul>
+            </div> <!-- end s-content__header -->
+    
+            <div class="s-content__media col-full">
+                <div class="s-content__post-thumb">
+                    <img src="/images/thumbs/featured/archery-tips.jpg">
+                </div>
+            </div> <!-- end s-content__media -->
+
+            <div class="col-full s-content__main">
+
+               <h2>Trigger Panic: What it is, and how to overcome it.</h2>
+
+                <p class="lead">Do you get the cold sweats when you're aiming your arrow down range? Mind starts racing as your arms quake a bit; slowly but surely it starts to consume your mind and your confidence. Let's break it down. What is the root of the problem? It's very important to understand where it comes from before you can begin learning proper shot execution. While there are many forms of trigger panic and just as many ways to cure it, there are a few things you can do to prevent or beat this horrible condition. </p>
+                
+               <p class="drop-cap">The main form of trigger panic derives from what I call 'anxious aiming.' This occurs when the pin settles in where you want it to and you feel like you have to fire that release immediately. Or, you start to slip into the habbit of touching off the trigger as the sight wobbles over the center of the target. Over time, you develop a hardwired connection between your trigger finger and your eye, and that connection takes time. Think about it, from the time your mind tells your finger to fire that shot, to the act of your finger actually doing it, is enough time to move off your mark.  Now, you may say you don’t have trigger panic, but if you can’t aim in the middle and slowly fire that shot, then I’ve got some bad news: YOU'VE GOT TRIGGER PANIC. Luckily, I'm here to help. </p>
+               
+               <h4>Know Thyself.</h4>
+               <p>You are probably wondering to yourself, "Self, how can I solve this? That's good! We want to use our brains, to get them to work for us. The fact is, the problem lies between your ears, not in the act of shooting. What you need to do is commit several weeks to the cure; every day or every chance you get, hold your release. It needs to become an extension of your hand. Get to know every click it makes, every scratch on it, everything about it. Know when it releases and exactly how much tension it takes to get there. Once you're comfortable with your new appendage, Move on to the next phase.</p>
+
+               <h4>Mental Reps.</h4>
+               <p>I don't care what it is, if you're wanting to improve, getting your mental reps in is key. I once heard a story of a Prisoner of War. Before the war, he was an avid golfer. After his liberation, when asked how he coped with everything, he replied that every day, he would play his favorite golf course in his head. Visualizing every part of his game and the the course. His first time back on the course, he shaved 4 strokes off his game! We will apply this same pricipal. When you're laying in bed, before you fall asleep, you need to think about your shot; EVERYTHING about your shot. From nocking the deathwhistle, to drawing your string, finding your target and putting just enough tenstion on your release to send that sucker down range. Watch your arrow go down the range, how is it flying? What kind of arch does it have? What's the weather like? Are you standing on dirt? Grass? A hill? You need to go over every detail in your mind. Send several arrows down range and hit your mark. Studies have shown us that as we sleep, our brains will work on and solve problems we were working on before we fell asleep. Let's use that to our advantage and work on our trigger panic in our sleep!</p>
+               
+               <h4>Let's Get Physical.</h4>
+               <p>So you've been doing your mental reps and you're ready to take the next step, well, go out in the yard or range with an arrow nocked, just like you would normally practice. Simply draw the bow and address the target, aiming with your finger on the trigger – but don’t pull that release; you want to focus only on aiming. Keep that pin where you want to the arrow to hit until your aiming starts to break down. Then let down and reset, never firing the arrow. Treat this just as you would any practice session, except never actually shoot your bow. This will do two things that benefit you greatly as an archer. First, it will teach your mind that it is OK for that pin to settle on the bulls-eye without firing that arrow. Second, it will increase the length of time during which you can effectively aim before your shot breaks down. Over time, you will be able to add shooting back into your routine, but if you ever feel those anxious or rushed feelings, take a few days and just commit to this simple drill.
+               </p>
+
+
+
+               <p>Trigger panic is a horrible thing, and if you don’t know how to cure it, it can really mess with your confidence, taking the fun out of shooting your bow. Even if you aren’t struggling with trigger panic (you do), these practices will help you stay calm, even in the most intense circumstances. 
+               </p>
+
+
+               <div class="s-content__author">
+                  <img src="/images/avatars/tucker.jpg" alt="">
+
+                  <div class="s-content__author-about">
+                     <h4 class="s-content__author-name">
+                        Tucker Brady
+                     </h4>
+                    
+                     <p> 
+                        A software engineer by trade, but first and foremost a husband and a father. When I'm not wrestling my two boys, or the bugs in my code, I'm up in the mountains. 
+                     </p>
+
+                     <ul class="s-content__author-social">
+                        <li><a href="https://www.facebook.com/tucker.a.brady" target="_blank">Facebook</a></li>
+                        <li><a href="https://twitter.com/tucker_brady" target="_blank">Twitter</a></li>
+                        <li><a href="https://www.instagram.com/tucker_al/" target="_blank">Instagram</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div> <!-- end s-content__main -->
+
+      </article>
+
+   </section> <!-- s-content -->
+
+    <!-- s-footer
+    ================================================== -->
+    <footer class="s-footer">
+
+        <div class="s-footer__main">
+            <div class="row">
+                
+                <div class="col-two md-four mob-full s-footer__sitelinks">
+                        
+                    <h4>Quick Links</h4>
+
+                    <ul class="s-footer__linklist">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="huskydivision.php">Husky Division</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                        <li><a href="#0">Privacy Policy</a></li>
+                    </ul>
+
+                </div> <!-- end s-footer__sitelinks -->
+
+                <div class="col-two md-four mob-full s-footer__social">
+                        
+                    <h4>Social</h4>
+
+                    <ul class="s-footer__linklist">
+                        <li><a href="https://www.facebook.com/mtnoutlaw/" target="_blank">Facebook</a></li>
+                        <li><a href="https://www.instagram.com/mtnoutlaw/" target="_blank">Instagram</a></li>
+                        <li><a href="https://twitter.com/MTNoutlaws" target="_blank">Twitter</a></li>
+                    </ul>
+
+                </div> <!-- end s-footer__social -->
+
+                <div class="col-five md-full end s-footer__subscribe">
+                        
+                    <h4>Our Newsletter</h4>
+
+                    <p>The Outlaws, direct in your mailbox once a month. No more... maybe less.</p>
+
+                    <form action="https://mtnoutlaw.us16.list-manage.com/subscribe/post?u=dd7bf8abc841427c769de79f5&amp;id=0e7eb4a916" name="cForm" id="cForm" method="post" action="">
+                        <input type="hidden" name="u" value="dd7bf8abc841427c769de79f5">
+                        <input type="hidden" name="id" value="0e7eb4a916">
+                    <fieldset>
+
+                        <div class="form-field">
+                            <input name="MERGE0" type="text" id="cEmail" class="full-width" placeholder="Your Email *" value="">
+                        </div>
+
+                        <button name="submit" type="submit" class="submit btn btn--primary full-width">Submit</button>
+                        
+                        <input type="hidden" name="recaptcha_response" id="recaptchaResponse">
+
+                        <p>
+                            This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy">Privacy Policy</a> and <a href="https://policies.google.com/terms">Terms of Service</a> apply.
+                        </p>
+
+                    </fieldset>
+                </form> <!-- end form -->
+
+                <?php // Check if form was submitted:
+                if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])) {
+
+                    // Build POST request:
+                    $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+                    $recaptcha_secret = '6LfHgvYUAAAAAIYO7EbVSakAs2-ypyr8f1hbYE-j';
+                    $recaptcha_response = $_POST['recaptcha_response'];
+
+                    // Make and decode POST request:
+                    $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+                    $recaptcha = json_decode($recaptcha);
+
+                    // Take action based on the score returned:
+                    if ($recaptcha->score >= 0.5) {
+                        // Verified - send email
+                    } else {
+                        // Not verified - show form error
+                    }
+
+                } ?><!--end recaptia/email submission php -->
+
+                </div> <!-- end s-footer__subscribe -->
+
+            </div>
+        </div> <!-- end s-footer__main -->
+
+        <div class="s-footer__bottom">
+            <div class="row">
+                <div class="col-full">
+                    <div class="s-footer__copyright">
+                        <span>© MTN Outlaws 2020</span> 
+                    </div>
+
+                    <div class="go-top">
+                        <a class="smoothscroll" title="Back to Top" href="#top"></a>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- end s-footer__bottom -->
+
+    </footer> <!-- end s-footer -->
+
+
+    <!-- preloader
+    ================================================== -->
+    <div id="preloader">
+        <div id="loader">
+            <div class="line-scale">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Java Script
+    ================================================== -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</php>
