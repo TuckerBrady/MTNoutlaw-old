9519c4c8e1d3d5e4c654d32ea5d573b6c86aba24
TuckerBrady <tuck.al.brady@gmail.com>
2021-07-20 20:34:24 -0600

    moved the single page templates.

---
+++ article-template.php
@@ -0,0 +1,93 @@
+<!DOCTYPE php>
+<php class="no-js" lang="en">
+<head>
+    <?php include "./pagehead.php" ?>
+</head>
+
+<body id="top">
+
+    <!-- pageheader
+    ================================================== -->
+    <div class="s-pageheader">
+
+        <?php include "../../header.html" ?>
+
+    </div> <!-- end s-pageheader -->
+
+
+    <!-- s-content
+    ================================================== -->
+    <section class="s-content s-content--narrow s-content--no-padding-bottom">
+
+        <article class="row format-standard">
+
+            <div class="s-content__header col-full">
+                <h1 class="s-content__header-title">
+                    Who We Are.
+                </h1>
+                <ul class="s-content__header-meta">
+                    <li class="date">January 1, 2020</li>
+                    <li class="cat">
+                        In
+                        <a href="/articles/lifestyle/lifestyle-articles.php">Lifestyle</a>
+                    </li>
+                </ul>
+            </div> <!-- end s-content__header -->
+    
+            <div class="s-content__media col-full">
+                <div class="s-content__post-thumb">
+                    <img src="/images/thumbs/single/standard/who-we-are.jpg">
+                </div>
+            </div> <!-- end s-content__media -->
+
+            <div class="col-full s-content__main">
+
+               <h2>The MTN Outlaws</h2>
+
+                <p class="lead">Sometimes the train to success needs to be held up, the mainstream needs to be dammed, and the road of the majority needs to be bridged.. Be an Outlaw.</p>
+                
+               <p class="drop-cap">The Mountain Outlaws was a process 20 years in the making. Whether it was trail rides on old motorcycles, camping in the hills above town, fishing alpine lakes and rivers, or hunting trips at the cabin, we’ve spent the majority of our time in the mountains. It’s only been over the past two years, however, that we (Mac and I) have sat down to really hammer out what we wanted the Outlaws to represent. </p>
+               
+               <p>I think he put it best:
+               There are millions and millions of dudes like us out there, guys who want to do fun things but don’t have the time or money to spend weeks out in the field. It is our responsibility as Mountain Outlaws to show those men that adventures can be had on a low budget or no budget. And that creativity and taking advantage of opportunities are tools to cherished. Any dummy can get online, read a few posts and spend his way into the club. But I don’t have $800 to spend on a pack, $400 to spend on boots, $1,000+ to spend on optics, and $1,200 to spend on my bow. I might have $600 to spend the entire year, on all my gear, for every adventure! But that’s the point, you can do it no matter how much you can spend. That’s who we are.</p>
+
+               <p>We make mistakes, we don’t catch every fish that hits our fly, we lose arrows from missed shots, and we fall down. From every misstep, arrow we let fly, and missed strike, we learn, we grow, and we adapt. We’re by no means professionals at what we do, we’re enthusiasts, just like you. We’re working hard everyday to get better at our craft. Whether thats choosing the bottle of water over the can of soda, going to the gym instead of watching that next episode (it’s most likely on Netflix anyway and will be waiting patiently for your return), or best case scenario, getting outside and answering the call of the wild. If we can inspire even just one person to step up and do one thing to improve their life in one way, Then we’ve been successful. That’s who we are.</p>
+               
+               <p>So feel free to stay a while, read some articles, contribute to some forums, feel free to ask some questions, and share what you’ve learned with the rest of us! After all, aren’t we all on the same side here? The Mountain Outlaws, it’s who WE are!
+               </p>
+
+               <?php include "../authors/tucker.html" ?>
+
+      </article>
+
+   </section> <!-- s-content -->
+
+    <!-- s-footer
+    ================================================== -->
+    <?php include "../../footer.php" ?>
+
+
+    <!-- preloader
+    ================================================== -->
+    <div id="preloader">
+        <div id="loader">
+            <div class="line-scale">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Java Script
+    ================================================== -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</php>
